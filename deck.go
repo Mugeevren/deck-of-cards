617dc11f645a7059042d5bd4cdb4bfa95af075af
MUGE BULUT <muge.bulut@daimler.com>
2021-12-08 12:38:51 +0300

    added comments about receiver

--- deck.go
+++ deck.go
@@ -6,6 +6,16 @@ import "fmt"
 //which is a slice of string
 type deck []string
 
+// usually reference with a one or two letter shortened version of the receiver argument
+// unlike any other languages, to use a one or two letter to define a variable is ok for go
+// instead of d we could use 'this' or 'self' and define the function like below,
+//It would compile, but it is breaking convention, go avoids any method of 'this' or 'self'
+// // func (this deck) print() {
+// // 	for i, card := range this {
+// // 		fmt.Println(i, card)
+// // 	}
+// // }
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
